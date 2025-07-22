356e09e34f1f651f38fc581e197dca6d56a3b016
Larry Saytee <saytee.software@gmail.com>
2025-07-22 23:18:12 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -12,16 +12,3 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  env: {
-    NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY: process.env.WEB3FORMS_ACCESS_KEY,
-    NEXT_PUBLIC_URL: process.env.URL,
-  }
-};
-
-export default nextConfig;
